fe95eec3242e9bc2087d94da3c85a0a808388238
Disezmike <mikenatchapon@gmail.com>
2025-05-14 17:00:17 +0700

    uplate mysql.js

--- utils/mysql.js
+++ utils/mysql.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -22,5 +13,5 @@ const params = {
     database: config_1.db_config.mysql.database,
     port: config_1.db_config.mysql.port
 };
-const Connect = () => __awaiter(void 0, void 0, void 0, function* () { return yield promise_1.default.createConnection(params); });
+const Connect = () => promise_1.default.createConnection(params);
 exports.Connect = Connect;
