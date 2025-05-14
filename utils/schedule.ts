ef35445cbf56b4bb5e2572c7477e0a33036c7435
Disezmike <mikenatchapon@gmail.com>
2025-05-14 14:59:36 +0700

    new Schedule method

--- utils/schedule.ts
+++ utils/schedule.ts
@@ -1,144 +1,154 @@
-import schedule from 'node-schedule';
+// import schedule from 'node-schedule';
 import { Connect } from './mysql';
+import nodeCron from 'node-cron';
 import { config } from './config';
 import { pushMessage } from './line';
 
-export const sendSchedule = async () => {
-	const conn = await Connect();
+interface TASK {
+	[propName: number]: any;
+}
 
-	conn.execute('SELECT * FROM groups').then(([results, fields]) => {
-		results = results as any[];
+interface Value {
+	id: number;
+	user_id: string;
+	title: string;
+	amount: number;
+	round: number;
+}
 
-		results.forEach(async (val, i) => {
-			try {
-				const group = val as any;
-				let sql = 'SELECT user_id FROM users WHERE `group`=?';
-				let [datas] = await conn.execute(sql, [(val as any).id]);
-				datas = datas as any[];
+let scheduledTasks:TASK = {}
 
-				if (datas.length == 0) return;
-				datas.forEach(async (data, i) => {
-					const user = data as any;
-                    const now =  new Date();
+export const fetchSchedulesAndSetJobs = async () => {
+	const connection = await Connect();
+	const [rows] = await connection.execute('SELECT users.id, user_id, `title`, amount, `round` from users INNER JOIN groups ON users.group=groups.id where active=1');
+	connection.end();
 
-                    const date = now.toLocaleDateString('th-TH', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: 'numeric',
-                      })
-					schedule.scheduleJob(`0 9 ${group.round} * *`, async () => {
-						console.log('Running Task!');
-						await pushMessage(config.channelAccessToken!, {
-							to: user.user_id,
-							messages: [
+	// Clear existing tasks
+	for (let id in scheduledTasks) {
+	  scheduledTasks[id].stop();
+	  delete scheduledTasks[id];
+	}
+
+	// Set new tasks
+	(rows as Value[]).forEach((schedule) => {
+		const now =  new Date();
+		const date = now.toLocaleDateString('th-TH', {
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+		})
+
+		const cronExpr = `* * ${schedule.round} * *`;
+
+		scheduledTasks[schedule.id] = nodeCron.schedule(cronExpr, async () => {
+		console.log(`Running task: ${schedule.id}`);
+		
+		await pushMessage(config.channelAccessToken!, {
+			to: schedule.user_id,
+			messages: [
+				{
+					type: 'flex',
+					altText: 'บิลมาแล้วจ้า',
+					contents: {
+						type: 'bubble',
+						body: {
+							type: 'box',
+							layout: 'vertical',
+							contents: [
 								{
-									type: 'flex',
-									altText: 'บิลมาแล้วจ้า',
-									contents: {
-										type: 'bubble',
-										body: {
-											type: 'box',
-											layout: 'vertical',
-											contents: [
-												{
-													type: 'box',
-													layout: 'vertical',
-													contents: [
-														{
-															type: 'text',
-															text: '🗓️ รอบเก็บเงินมาแล้ว!',
-															weight: 'bold',
-															size: 'lg',
-															color: '#C7253E',
-															align: 'start',
-														},
-														{
-															type: 'text',
-															text: `วันที่ ${date}`,
-															offsetTop: '10px',
-															color: '#888888',
-														},
-													],
-													paddingBottom: '10px',
-												},
-												{
-													type: 'separator',
-													margin: '10px',
-												},
-												{
-													type: 'box',
-													layout: 'baseline',
-													spacing: 'sm',
-													contents: [
-														{
-															type: 'text',
-															text: `${group.name}`,
-															flex: 2,
-															color: '#C7253E',
-														},
-														{
-															type: 'text',
-															text: 'ราคา',
-															color: '#888888',
-															size: 'sm',
-															flex: 2,
-															align: 'end',
-															offsetEnd: '20px',
-															gravity: 'bottom',
-														},
-														{
-															type: 'text',
-															text: `฿${group.amount}`,
-															wrap: true,
-															color: '#009900',
-															size: 'xxl',
-															flex: 2,
-															gravity: 'bottom',
-														},
-													],
-													paddingAll: '5px',
-												},
-												{
-													type: 'separator',
-													margin: '10px',
-												},
-											],
+									type: 'box',
+									layout: 'vertical',
+									contents: [
+										{
+											type: 'text',
+											text: '🗓️ รอบเก็บเงินมาแล้ว!',
+											weight: 'bold',
+											size: 'lg',
+											color: '#C7253E',
+											align: 'start',
 										},
-										footer: {
-											type: 'box',
-											layout: 'vertical',
-											spacing: 'sm',
-											contents: [
-												{
-													type: 'button',
-													style: 'link',
-													action: {
-														type: 'message',
-														label: 'จ่ายเลย!',
-														text: 'จ่ายเงิน',
-													},
-													height: 'sm',
-													offsetTop: '0px',
-													color: '#C7253E',
-												},
-											],
-											flex: 0,
-											paddingTop: '0px',
-											paddingStart: '0px',
-											paddingEnd: '0px',
-											paddingBottom: '10px',
+										{
+											type: 'text',
+											text: `วันที่ ${date}`,
+											offsetTop: '10px',
+											color: '#888888',
 										},
-									},
+									],
+									paddingBottom: '10px',
+								},
+								{
+									type: 'separator',
+									margin: '10px',
+								},
+								{
+									type: 'box',
+									layout: 'baseline',
+									spacing: 'sm',
+									contents: [
+										{
+											type: 'text',
+											text: `${schedule.title}`,
+											flex: 2,
+											color: '#C7253E',
+										},
+										{
+											type: 'text',
+											text: 'ราคา',
+											color: '#888888',
+											size: 'sm',
+											flex: 2,
+											align: 'end',
+											offsetEnd: '20px',
+											gravity: 'bottom',
+										},
+										{
+											type: 'text',
+											text: `฿${schedule.amount}`,
+											wrap: true,
+											color: '#009900',
+											size: 'xxl',
+											flex: 2,
+											gravity: 'bottom',
+										},
+									],
+									paddingAll: '5px',
+								},
+								{
+									type: 'separator',
+									margin: '10px',
 								},
 							],
-						});
-					});
-				});
-			} catch (e: any) {
-				console.log(e.response.data);
-			}
+						},
+						footer: {
+							type: 'box',
+							layout: 'vertical',
+							spacing: 'sm',
+							contents: [
+								{
+									type: 'button',
+									style: 'link',
+									action: {
+										type: 'message',
+										label: 'จ่ายเลย!',
+										text: 'จ่ายเงิน',
+									},
+									height: 'sm',
+									offsetTop: '0px',
+									color: '#C7253E',
+								},
+							],
+							flex: 0,
+							paddingTop: '0px',
+							paddingStart: '0px',
+							paddingEnd: '0px',
+							paddingBottom: '10px',
+						},
+					},
+				},
+			],
 		});
-
-		conn.end();
+	  });
 	});
-};
+  
+	console.log('Schedules updated.');
+}
