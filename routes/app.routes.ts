2e6e32bff6941c77b8de998286c5054382fa8fbd
Disezmike <mikenatchapon@gmail.com>
2025-05-14 16:02:15 +0700

    .

--- routes/app.routes.ts
+++ routes/app.routes.ts
@@ -4,8 +4,6 @@ import { config } from '../utils/config';
 
 import * as controller from '../controllers/app.controller';
 
-import { Connect } from '../utils/mysql';
-
 const router = Router();
 
 router.get('/', (req, res) => {
