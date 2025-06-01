84b7a00b10844fdd6fbe8735f69fcb0b35387016
Disezmike <mikenatchapon@gmail.com>
2025-06-01 14:11:50 +0700

    change port

--- backend/web/src/config/constant.ts
+++ backend/web/src/config/constant.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:8080";
+export const API_URL = `http://localhost:${import.meta.env.PORT}`;
 // export const API_URL = "https://sandniax.mikenatcavon.com";
 
 export type Message = {
