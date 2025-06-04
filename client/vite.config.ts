a9c6629fb9eaeec9dd2918efc4d8b07140d12b72
Disezmike <mikenatchapon@gmail.com>
2025-06-04 19:58:41 +0700

    add preview config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig({
     port: 3000,
     allowedHosts: true
   },
+  preview: {
+    port: 3000,
+    allowedHosts: true,
+  },
   build: {
     chunkSizeWarningLimit: 1600
   }
